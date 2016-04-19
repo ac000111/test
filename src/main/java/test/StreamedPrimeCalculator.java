eb46ffb50acbbf314c8066a81d93263efc69a6c1
andrew <ac000111@yahoo.com>
2016-04-19 10:24:24 +0100

    Use a callback handler as the public interface instead

--- src/main/java/test/StreamedPrimeCalculator.java
+++ src/main/java/test/StreamedPrimeCalculator.java
@@ -1,13 +1,16 @@
 package test;
 
-import java.io.PipedWriter;
+import java.util.function.Consumer;
 
 /**
- * An implementation that writes values as soon as they're calculated to the given writer, without
+ * An implementation that calls the given callback function as soon as a prime number is found,
  * waiting for all calculations to be completed.
+ * <br>
+ * Each callback invocation may be performed on a different thread, so the callback handler must
+ * be thread-safe.
  */
 public class StreamedPrimeCalculator implements PrimeCalculator {
-    private PipedWriter writer;
+    private Consumer<Integer> callback;
 
     @Override
     // TODO
